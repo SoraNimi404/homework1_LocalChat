b902c05f6f09f278c2c53340e0188f7431d9b803
ikunofcxk <1079581675@qq.com>
2025-04-17 16:37:21 +0800

    爆改服务器代码，取消私聊功能，添加进程守护

--- Server/Server.c
+++ Server/Server.c
@@ -9,117 +9,181 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <signal.h>
+#include <errno.h>
 
-#define BUF_SIZE (4096)
-#define MAX_CLIENTS (20)
+#define PORT 8080              // 服务器监听的端口号
+#define MAXCLIENTS 100         // 最大客户端连接数
+#define BUFFERSIZE 2048        // 数据缓冲区大小
 
-sem_t human_number; // 当前聊天人数
+int clientsockets[MAXCLIENTS]; // 存储客户端套接字数组
+int nclients = 0;              // 当前连接的客户端数量
 
-// 服务端文件描述符
-//又称socket描述字
-int svr_fd;
+pthread_mutex_t clientsmutex = PTHREAD_MUTEX_INITIALIZER; // 客户端数组互斥锁
 
-int cli_fd[MAX_CLIENTS + 1] = {}; // 群友数量，多一个机器人负责播报
+// 添加客户端到客户端数组
+void addclient(int socket) {
+    pthread_mutex_lock(&clientsmutex); // 加锁以保护客户端数组
+    for (int i = 0; i < MAXCLIENTS; i++) {
+        if (clientsockets[i] == 0) {
+            clientsockets[i] = socket; // 添加客户端套接字
+            nclients++; // 增加客户端计数
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clientsmutex); // 解锁
+}
 
+// 从客户端数组中移除客户端
+void removeclient(int socket) {
+    pthread_mutex_lock(&clientsmutex); // 加锁以保护客户端数组
+    for (int i = 0; i < MAXCLIENTS; i++) {
+        if (clientsockets[i] == socket) {
+            clientsockets[i] = 0; // 移除客户端套接字
+            nclients--; // 减少客户端计数
+            break;
+        }
+    }
+    pthread_mutex_unlock(&clientsmutex); // 解锁
+}
 
-struct client{
-	/* data */
-	char buf[BUF_SIZE];	 // message
-	char name[BUF_SIZE]; // name
-	int client_fd;		 // fd
-};
+// 广播消息给所有客户端
+void broadcastmessage(char *message, int sendersocket) {
+    pthread_mutex_lock(&clientsmutex); // 加锁以保护客户端数组
+    for (int i = 0; i < MAXCLIENTS; i++) {
+        if (clientsockets[i] != 0 && clientsockets[i] != sendersocket) {
+            send(clientsockets[i], message, strlen(message), 0); // 发送消息
+        }
+    }
+    pthread_mutex_unlock(&clientsmutex); // 解锁
+}
+
+// 处理客户端消息的线程函数
+void *handleclient(void *socketptr) {
+    int socket = *(int *)socketptr;
+    free(socketptr); // 释放分配的内存
+
+    char buffer[BUFFERSIZE];
+    int readsize;
+
+    // 循环接收客户端消息
+    while ((readsize = recv(socket, buffer, BUFFERSIZE, 0)) > 0) {
+        buffer[readsize] = '\0'; // 确保字符串结束
+        broadcastmessage(buffer, socket); // 广播消息
+    }
+
+    if (readsize == 0) {
+        printf("Client disconnected: %d\n", socket);
+    } else if (readsize == -1) {
+        perror("recv failed");
+    }
+
+    removeclient(socket); // 移除客户端
+    close(socket); // 关闭套接字
+    return NULL;
+}
+
+// 设置守护进程
+void daemonize() {
+    pid_t pid = fork(); // 第一次fork
+
+    if (pid < 0) {
+        exit(EXIT_FAILURE); // fork失败，退出
+    }
+
+    if (pid > 0) {
+        exit(EXIT_SUCCESS); // 父进程退出
+    }
+
+    if (setsid() < 0) { // 创建新的会话
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGCHLD, SIG_IGN); // 忽略子进程状态变化信号
+    signal(SIGHUP, SIG_IGN); // 忽略挂起信号
+
+    pid = fork(); // 第二次fork
+
+    if (pid < 0) {
+        exit(EXIT_FAILURE); // fork失败，退出
+    }
+
+    if (pid > 0) {
+        exit(EXIT_SUCCESS); // 父进程退出
+    }
+
+    // 设置文件权限掩码
+    umask(0);
+
+    // 更改工作目录
+    chdir("/");
+
+    // 关闭所有文件描述符
+    for (int i = sysconf(_SC_OPEN_MAX); i >= 0; i--) {
+        close(i);
+    }
+}
 
 int main() {
-    // 初始化信号量
-    if (sem_init(&human_number, 0, 0) != 0) {
-        perror("sem_init");
-        return -1;
+    int serversocket, newsocket;
+    struct sockaddr_in serveraddr, clientaddr;
+    socklen_t clientaddrsize;
+    pthread_t threadid;
+
+    daemonize(); // 设置为守护进程
+
+    // 创建Socket
+    serversocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serversocket == -1) {
+        perror("Could not create socket");
+        return 1;
     }
 
-    // 创建socket对象
-    printf("[system]创建socket对象...\n");
-    svr_fd = socket(AF_INET, SOCK_STREAM, 0);
-    //AF_INET ipv4协议，SOCK_STREAM 基于有保障的链接方式，这里是TCP 0则是与第二个参数同步
-    if ( svr_fd < 0) {
-        perror("socket");
-        sem_destroy(&human_number); // 销毁信号量
-        return -1;
+    // 设置服务器地址结构
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
+    serveraddr.sin_port = htons(PORT);
+
+    // 绑定Socket到地址
+    if (bind(serversocket, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
+        perror("bind failed");
+        return 1;
     }
-    //端口复用函数：解决端口号被系统占用的情况
-	int on = 1;
-	int gg = setsockopt(svr_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-	if(gg==-1)
-	{
-		perror("setsockopt");
-		return -1;
-	}
- 
-	// 准备通信地址(自己)
-	printf("[system]准备通信地址...\n");
-	struct sockaddr_in addr = {};
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(6666);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	socklen_t addrlen = sizeof(addr);
- 
-	// 绑定socket对象与地址
-	printf("[system]绑定socket对象与地址...\n");
-	if (bind(svr_fd, (struct sockaddr *)&addr, addrlen)){
-		perror("bind");
-		return -1;
-	}
- 
-	// 设置监听和排除数量
-	printf("[system]设置监听");
-	if (listen(svr_fd, 10)){
-		perror("listen");
-		return -1;
-	}
- 
-	printf("[system]等待客户端链接...\n");
-	// 将初始值置全为-1，表示该聊天位置没有人占领
-	memset(cli_fd, -1, sizeof(cli_fd));
-	while(1){
-		int sem_num;
-		sem_getvalue(&human_number, &sem_num);
- 
-		// 找到没有人占领的聊天位
-		int index = 0;
-		while (-1 != cli_fd[index])
-			index++;
-		cli_fd[index] = accept(svr_fd, (struct sockaddr *)&addr, &addrlen);
- 
-		if (0 > cli_fd[index])
-		{
-			perror("accept");
-			return -1;
-		}
- 
-		char buf[BUF_SIZE];
-		if (0 >= sem_num)
-		{
-			printf("[system]人数已满，%d号客户端链接失败\n", cli_fd[index]);
-			sprintf(buf, "[system]人数已满，客户端链接失败");
-			write(cli_fd[index], buf, strlen(buf) + 1);
-			close(cli_fd[index]);
-			cli_fd[index] = -1;
-		}
-		else
-		{
-			sem_trywait(&sem);
-			sem_getvalue(&sem, &sem_num);
-			char msg[BUF_SIZE] = {};
-			printf("[system]%d号客户端链接成功,当前聊天人数%d人\n", cli_fd[index], SEM_SIZE - sem_num);
-			sprintf(msg, "[system]客户端链接成功,当前聊天人数%d人\n", SEM_SIZE - sem_num);
-			write(cli_fd[index], msg, strlen(msg) + 1);
- 
-			// 创建线程客户端
-			pthread_t tid;
-			pthread_create(&tid, NULL, server, &cli_fd[index]);
-		}
-	}
-    
-    sem_destroy(&human_number);
+
+    // 开始监听
+    if (listen(serversocket, MAXCLIENTS) < 0) {
+        perror("listen failed");
+        return 1;
+    }
+
+    printf("Waiting for incoming connections...\n");
+
+    // 接受连接循环
+    while ((newsocket = accept(serversocket, (struct sockaddr *)&clientaddr, &clientaddrsize))) {
+        if (newsocket < 0) {
+            perror("accept failed");
+            continue;
+        }
+
+        printf("Connection accepted: %d\n", newsocket);
+
+        int *newsock = malloc(sizeof(int));
+        *newsock = newsocket;
+
+        addclient(newsocket);
+
+        // 为新客户端创建处理线程
+        if (pthread_create(&threadid, NULL, handleclient, (void *)newsock) < 0) {
+            perror("Could not create thread");
+            return 1;
+        }
+
+        pthread_detach(threadid); // 分离线程，不等待线程结束
+    }
+
+    if (newsocket < 0) {
+        perror("Accept failed");
+        return 1;
+    }
 
     return 0;
 }
